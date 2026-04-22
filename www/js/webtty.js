892affb587408ff5678251c0be236a7d8e655c6f
Chris Mason <chris@netnix.org>
2026-04-22 10:30:54 +0100

    update

--- www/js/webtty.js
+++ www/js/webtty.js
@@ -1,6 +1,11 @@
 (() => {
   window.addEventListener('load', (e) => {
-    const t = new Terminal();
+    const t = new Terminal({
+      theme: {
+        foreground: '#b3b0d6',
+        background: '#29283b'
+      }
+    });
     t.open(document.getElementById('terminal'));
     t.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
   });
